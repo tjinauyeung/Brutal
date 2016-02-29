2072302f732535795dc38ba80f6a46d3dd7f5b81
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-02-29 10:57:25 +0100

    added methods to the controller

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,47 @@
 class BuildingsController < ApplicationController
-	def index
-	end
+
+  def index
+    render json:{
+      buildings: Building.order(id: :asc)
+    }
+  end
+
+  def create
+    if building = Building.create(project_params)
+      render json:{building: building}
+    else
+      render json: {
+        message: "Sorry, building could not be created",
+        errors: building.errors
+      }
+    end
+  end
+
+  def update
+    if building = Building.find(params[:id])
+      render json:{building: building.update(building_params)}
+    else
+      render json: {
+        message:"Sorry, project could not be edited",
+        errors: building.errors
+      }
+    end
+  end
+
+  def destroy
+    building = Building.find(params[:id])
+    building.destroy
+      render json: { destroyed: true }
+  end
+
+  def show
+    building = Building.find(params[:id])
+      render json:{building: building}
+  end
+
+  private
+  def building_params
+    params.require(:building).permit(:name, :location, :build_year, :architect, :rating)
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,50 @@
 class ReviewsController < ApplicationController
+before_action :set_building
+  
+  def index
+    render json:{
+      reviews: @building.review.order(id: :desc)
+    }
+  end
+
+  def create
+    review = Review.new(review_params)
+    @building = review.building
+
+    if review.save
+      render json: {review: review}
+    else
+      render json: {
+        message: "Sorry, review could not be created",
+        errors: review.errors
+      }
+    end
+  end
+
+  def update
+    if review = @building.reviews.find(params[:id])
+    render json:{review: review.update(task_params)}
+    else
+      render json: {
+        message:"Sorry, review could not be edited",
+        errors: review.errors
+      }
+    end
+  end
+
+  def destroy
+    review = @building.reviews.find(params[:id])
+    task.destroy
+    render json: { destroyed: true }
+  end
+  private
+
+  def set_building
+    @building = Building.find(params[:building_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:name, :description, :building_id)
+  end
+
 end
