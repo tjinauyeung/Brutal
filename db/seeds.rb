e61e39b77bf640aa8a30422e79d497280b7f76a4
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-03-20 12:46:15 +0100

    added three more buildings in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,23 @@ Building.create([
 
 	{ name: 'El Colegio de Mexico', location: 'Mexico City, MEX', build_year: '1975', architect: 'Abraham Zabludovsky', image: "http://36.media.tumblr.com/e5333721f8ebced0687a0795b05cc17e/tumblr_o10150uLSf1qzglyyo1_1280.jpg" },
 
-	{ name: 'Genex Tower', location: 'Belgrade, SRB', build_year: '', architect: 'Mihajlo Mitrović', image: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Jugotours_Beograd_Dec_2003.jpg" },
+	{ name: 'Genex Tower', location: 'Belgrade, SRB', build_year: '', architect: 'Mihajlo Mitrović', image: "https://s-media-cache-ak0.pinimg.com/1200x/87/a5/93/87a5938bf3bbbe79f0519e36a960de03.jpg" },
 
 	{ name: 'Forum Hotel', location: 'Krakow, PL', build_year: '', architect: '', image: "http://36.media.tumblr.com/17f94b32659e9539e442f5f34c700230/tumblr_n1dinjpqya1suhlbho1_1280.jpg" },
 
 	{ name: 'Druzhba Holiday Center Hal', location: 'Yalta, RUS', build_year: '', architect: '', image: "https://s-media-cache-ak0.pinimg.com/736x/54/91/d8/5491d8e40b6a2efa1104e9a912d02ace.jpg" },
 
-	{ name: 'Poplakov Cafe', location: 'UKR', build_year: '1976', architect: '', image: "http://weirdrussia.com/wp-content/uploads/2015/04/Het-Poplakov-Cafe-1976.jpg" },
+	{ name: 'Water Tower', location: '', build_year: '', architect: '', image: "http://blog.presentandcorrect.com/wp-content/uploads/2012/07/RIMG0180.jpg" },
 
-	{ name: 'Hotel Uzbekistan', location: 'Tasjkent, UZ', build_year: '1974', architect: 'Steven Holl', image: "https://c2.staticflickr.com/8/7710/16502697923_118a12bd55_b.jpg" },
+	{ name: 'Gottfried Church', location: 'Gottfried, GER', build_year: '', architect: '', image: "https://s-media-cache-ak0.pinimg.com/736x/a8/a9/ef/a8a9ef536b74ec315ce9aa7e937b8554.jpg" },
 
 	{ name: 'Metropolitan Cathedral', location: 'Brasilia, BRA', build_year: '1960', architect: 'Oscar Niemeyer', image: "http://static.thecitypictures.net/cache/1410x1440-3/cathedral-metropolitana-of-brasilia.jpg" },
+
+	{ name: 'Washington Hilton Hotel', location: 'Washington, US', build_year: '', architect: '', image: "https://c2.staticflickr.com/8/7087/7294765280_ed5cb79ec2_b.jpg" },
+
+	{ name: 'College Life Insurance Company Headquarters,', location: 'Indianapolis, US', build_year: '', architect: '', image: "http://40.media.tumblr.com/tumblr_lmkzckZROt1qzglyyo1_1280.jpg" },
+
+	{ name: '', location: '', build_year: '', architect: '', image: "https://s-media-cache-ak0.pinimg.com/736x/49/84/82/4984825e618fb70294317917f700aa54.jpg" },
 
 	]) 
 
